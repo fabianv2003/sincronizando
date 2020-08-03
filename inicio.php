f8372a93cad92bc273e5b718e3774d085f840a27
fabianv <fabianv2003@gmail.com>
2020-08-02 23:15:43 -0400

    Segundo Cambio

--- inicio.php
+++ inicio.php
@@ -9,5 +9,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="google.cl">
     </head>
+    <script>
+      let ejemplo=12;
+    
+    </script>
 
 </html>
