3a5873b52f33140693b979536aadf2b1d7c560d7
fabianv <fabianv2003@gmail.com>
2020-08-20 09:45:56 -0400

    Subir cambio 1 para generar un conflicto

--- contacto.php
+++ contacto.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>subir 11</title>
+    <title>Cambio en GIT 2</title>
 </head>
 <body>
     
